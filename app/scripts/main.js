97dfe129aaff6413cf5c82b4351865e6c4979d3b
Francesco Fontana <francesco.fontana@gmail.com>
2013-05-02 21:03:47 +0200

    background image and handlebars

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 require.config({
     paths: {
         jquery: '../components/jquery/jquery',
-        bootstrap: 'vendor/bootstrap'
+        bootstrap: 'vendor/bootstrap',
+        handlebars: '../components/handlebars/handlebars'
     },
     shim: {
         bootstrap: {
@@ -11,9 +12,15 @@ require.config({
     }
 });
 
-require(['app', 'jquery', 'bootstrap'], function (app, $) {
+require(['app', 'jquery', 'bootstrap','handlebars'], function (app, $) {
     'use strict';
     // use app here
     console.log(app);
     console.log('Running jQuery %s', $().jquery);
+
+		//compile the template
+    var source = document.getElementById("photo-template").textContent;
+    var myTemplate = Handlebars.compile(source);
+    var contentHTML = myTemplate({ didascalia : 'prova didascalia' });
+		document.getElementById("photo-background").innerHTML = contentHTML;
 });
